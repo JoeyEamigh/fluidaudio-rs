85f5d077ae5ba032bce214538c7bb4e756a99e55
Alex-Wengg <hanweng9@gmail.com>
2026-01-14 22:50:52 -0500

    Sync version to FluidAudio 0.10.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/FluidInference/FluidAudio.git", from: "0.7.8"),
+        .package(url: "https://github.com/FluidInference/FluidAudio.git", exact: "0.10.0"),
     ],
     targets: [
         .target(
